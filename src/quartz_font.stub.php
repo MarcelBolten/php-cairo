d1249615252f8e821a215065a8a86f1d88eb3e5d
Marcel Bolten <github@marcelbolten.de>
2025-09-15 17:15:43 -0700

    add stub and arginfo

---
+++ src/quartz_font.stub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cairo\FontFace;
+
+class Quartz extends \Cairo\FontFace
+{
+    public static function createForCGFont(
+        string $fontName
+    ): Quartz {}
+}
---
+++ src/quartz_font_arginfo.h
@@ -0,0 +1,7 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 39b5e5a646852a887018783fd70fbc637161937a */
+
+ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_class_Cairo_FontFace_Quartz_createForCGFont, 0, 1, Cairo\\FontFace\\Quartz, 0)
+	ZEND_ARG_TYPE_INFO(0, fontName, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
